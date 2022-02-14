9587311534fb99f0475a80f64c7f42dc3cb18191
jungcolor <jungcolor@gmail.com>
2022-02-14 22:19:46 +0900

    logout 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,28 @@
+import axios from 'axios';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+    const navigate = useNavigate(); // v5 이상
+    const onClickHandler = (event) => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    navigate('/login');
+                }
+                else {
+                    alert('로그아웃 하는데 실패했습니다.');
+                }
+            });
+    }
+
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
         }}>
             <h2>시작페이지</h2>
+
+            <button onClick={onClickHandler}>로그아웃</button>
         </div>
     );
 }
